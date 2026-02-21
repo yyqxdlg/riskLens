6dfe9d16aab5d20dcaa03aeab9ca27dc202f7bfc
Yu Zhang <yuz2@kth.se>
2026-02-21 20:16:00 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <DashBoard msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import DashBoard from './components/DashBoard.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
+    DashBoard
   }
 }
 </script>
