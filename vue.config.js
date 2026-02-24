729d955a6db3d9eecac4400fde2e5700d6ace579
Yu Zhang <yuz2@kth.se>
2026-02-24 20:30:51 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/riskLens/'
+    : '/',
+    
+  // 如果你想让打包后的文件直接输出到 docs 文件夹而不是 dist
+  // 这样你提交代码后，GitHub Pages 就能直接识别
+  outputDir: 'docs'
 })
